32a39f6134cd13985055fd3f70d2b930851c8598
Gesko927 <gesko927@gmail.com>
2017-04-12 22:35:03 +0300

    Add Chapter 4 Add Chapter 5

--- Chapter 4/Program.cs
+++ Chapter 4/Program.cs
@@ -2,20 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Chapter_4
 {
     class Program
     {
+        static ConsoleKeyInfo keyInfo;
         static void Main(string[] args)
         {
+            Thread st1 = new Thread(PrintPrimes);
+
+            st1.Start();
+
+            do
+            {
+                keyInfo = Console.ReadKey();
+                if(keyInfo.Key == ConsoleKey.P)
+                {
+                    st1.Suspend();
+                }
+                else if(keyInfo.Key == ConsoleKey.R)
+                {
+                    st1.Resume();
+                }
+            }
+            while (keyInfo.Key != ConsoleKey.Escape);
+
+            st1.Abort();
 
         }
 
         static void PrintPrimes()
         {
+            int n = 10000;
+            bool[] A = new bool[n];
 
+            for (int i = 2; i < n; i++)
+            {
+                A[i] = true;
+            }
+
+            for (int i = 2; i < Math.Sqrt(n) + 1; ++i)
+            {
+                if (A[i])
+                {
+                    for (int j = i * i; j < n; j += i)
+                    {
+                        A[j] = false;
+                    }
+                }
+            }
+         
+            for (int i = 2; i < n; i++)
+            {
+                if (A[i])
+                {
+                    Console.WriteLine("{0} ", i);
+                    Thread.Sleep(500);
+                }
+            } 
+            
         }
     }
 }
---
+++ Chapter 5/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chapter_5
+{
+    class Program
+    {
+        #region ManualResetEvent
+        class MyThread
+        {
+            public Thread Thrd;
+
+            ManualResetEvent mre;
+
+            public MyThread(string name, ManualResetEvent evt)
+            {
+                Thrd = new Thread(this.Run);
+                Thrd.Name = name;
+                mre = evt;
+                Thrd.Start();
+            }
+
+            void Run()
+            {
+                Console.WriteLine("In " + Thrd.Name);
+
+                for (int i = 0; i < 5; i++)
+                {
+
+                    Console.WriteLine(i.ToString());
+                    Thread.Sleep(500);
+                }
+
+                Console.WriteLine(Thrd.Name + " finish!");
+
+                mre.Set();
+            }
+        }
+
+        static void Main()
+        {
+            ManualResetEvent evtObj = new ManualResetEvent(false);
+
+            MyThread mt1 = new MyThread("Thread #1", evtObj);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Main thread waiting for event");
+
+            evtObj.WaitOne();
+
+            Console.WriteLine("Main thread got signal from FIRST thread");
+
+            evtObj.Reset();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            mt1 = new MyThread("Thread #2", evtObj);
+
+            Console.WriteLine("Main thread waiting for event");
+            evtObj.WaitOne();
+
+            Console.WriteLine("Основной поток получил уведомление о событии от второго потока");
+        }
+        #endregion
+
+        #region AutoResetEvent
+
+        //class MyThread
+        //{
+        //    public Thread Thrd;
+
+        //    AutoResetEvent are;
+
+
+        //    public MyThread(string name, AutoResetEvent evt)
+        //    {
+        //        Thrd = new Thread(this.Run);
+        //        Thrd.Name = name;
+        //        are = evt;
+        //        Thrd.Start();
+        //    }
+
+        //    void Run()
+        //    {
+        //        Console.WriteLine("In " + Thrd.Name);
+
+        //        for (int i = 0; i < 5; i++)
+        //        {
+        //            Console.WriteLine(i.ToString());
+        //            Thread.Sleep(500);
+        //        }
+
+        //        Console.WriteLine(Thrd.Name + " finish!");
+
+
+        //        are.Set();
+        //    }
+        //}
+
+        //static void Main()
+        //{
+        //    AutoResetEvent evtObj = new AutoResetEvent(false);
+
+        //    MyThread mt1 = new MyThread("Thread #1", evtObj);
+
+        //    Console.ForegroundColor = ConsoleColor.Green;
+
+        //    Console.WriteLine("Main thread waiting for event");
+        //    evtObj.WaitOne();
+
+        //    Console.WriteLine("Main thread got signal from FIRST thread");
+
+        //    Console.ForegroundColor = ConsoleColor.Red;
+
+        //    mt1 = new MyThread("Thread #2", evtObj);
+
+        //    Console.WriteLine("Main thread waiting for event");
+
+        //    evtObj.WaitOne();
+
+        //    Console.WriteLine("Main thread got signal from SECOND thread");
+        //}
+
+        #endregion
+    }
+}
+
